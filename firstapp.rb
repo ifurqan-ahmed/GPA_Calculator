8ffd60cc3d6c8bdb9257da2443bf0d78ce9a149d
Furqan Ahmed <fam4792@gmail.com>
2014-02-15 16:22:51 +0500

    Grades and refined

--- firstapp.rb
+++ firstapp.rb
@@ -1,43 +1,72 @@
-class Array
-    def sum
-        return self.inject(:+)
-    end
-end
+GRADES = {"a" => 4, "b+" => 3.5, "b" => 3, "c+" => 2.5 , "c" => 2 , "d+" => 1.5, "d" => 1, "f" => 0 }
 
 def gt
     return gets.chomp
 end
 
+def nextl
+    return puts ""
+end
 
-p "My GPA Calculator"
+def lines
+    return"###############################################################"
+end
 
+
+
+def inpgr
+
+    x=gt
+
+    while not GRADES.keys.include? x.downcase  
+        print"Invalid Entry. Try Again : "
+        x=gt
+    end
+
+        x.downcase!
+        return GRADES[x]
+end
+
+
+
+p "################### My GPA Calculator #####################"
+
+nextl
 print "Enter no. of subjects: "
 num = gt.to_i
+nextl
 
 gp=[]
 cr=[]
 
 num.times do |i|
-    print "Enter the grade in subject #{i+1}: "
-    gp[i]= gt.to_f
 
-    print "Enter the credit hours of subject #{i+1}: "
+    puts"##################### Subject #{i+1} ############################"
+    
+    nextl
+
+    print "Enter the grade in this subject: "
+    gp[i]= inpgr
+    nextl
+
+    print "Enter the credit hours of this subject: "
     cr[i]=gt.to_f
 
-    puts ""
+    nextl
+    nextl
 end
 
-tcr=cr.sum
+tcr=cr.inject(:+)
 
-f=0
+n=0
 
 num.times do |i|
-    f += (gp[i]*cr[i])
+    n += (gp[i]*cr[i])
 end
 
-gpa = f/tcr
+gpa = n/tcr
 
-p "Your GPA is : #{gpa}"
+p "################## Your GPA is : #{gpa} ######################"
 
 
 
